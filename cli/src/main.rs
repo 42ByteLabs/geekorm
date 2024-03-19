032fadee81eda10f837d7619b2d0eeedfe31b9fd
GeekMasher <geekmasher@gmail.com>
2024-03-19 21:49:33 +0000

    feat: Update CLI with examples

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,14 @@
 #![allow(unused_imports, dead_code, unused_variables)]
 use anyhow::Result;
-use geekorm::{prelude::*, queries::QueryOrder};
+// Imports
+use geekorm::prelude::*;
 use geekorm_derive::GeekTable;
 
-#[derive(Debug, GeekTable)]
+#[derive(Debug, Default, GeekTable)]
 pub struct User {
     pub name: String,
     pub age: i32,
+    pub postcode: Option<String>,
 }
 
 fn main() -> Result<()> {
@@ -16,19 +18,32 @@ fn main() -> Result<()> {
     println!("Table :: {:?}\n", table);
 
     let query = User::create().build()?;
-    println!("Create :: `{}`", query);
+    println!("Create :: `{}`\n", query);
 
     let select = User::select()
+        .where_ne("name", "Mathew")
+        // .and()
+        .and()
+        .where_gt("age", 20)
+        .and()
+        .where_lt("age", 30)
         // order by name asc
         .order_by("age", QueryOrder::Asc)
         // build
         .build()?;
 
     println!("Select :: `{}`", select);
+    println!("Values :: {:?}\n", select.values);
 
-    // let user = User::select()
-    //     .where(User::Column::name == "John" || (User::Column::age > 20 && User::Column::age < 30))
-    //     .execute()?;
+    let count = User::count().build()?;
+    println!("Count  :: `{}`", count);
+
+    let limit = User::select()
+        .order_by("age", QueryOrder::Asc)
+        .limit(10)
+        .offset(0)
+        .build()?;
+    println!("Limit  :: `{}`", limit);
 
     Ok(())
 }
