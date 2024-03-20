df8ccbf94a0aff3ceaacba629002e29fcf6df56f
GeekMasher <geekmasher@gmail.com>
2024-03-20 18:01:57 +0000

    feat(examples): Add users example

---
+++ examples/users/src/main.rs
@@ -0,0 +1,29 @@
+#![allow(dead_code)]
+
+use geekorm::{GeekTable, TableBuilder};
+
+#[derive(Debug, Clone, GeekTable)]
+struct User {
+    username: String,
+    email: String,
+    active: bool,
+    postcode: Option<String>,
+}
+
+fn main() {
+    // Build a CREATE TABLE query
+    let create_query = User::create().build().expect("Failed to build query");
+
+    println!("Create Query: {}", create_query);
+
+    // Build a SELECT query with WHERE, OR and LIKE
+    let select_query = User::select()
+        .where_eq("username", "geekmasher")
+        .or()
+        .where_like("email", "%geekmasher%")
+        .build()
+        .expect("Failed to build query");
+
+    println!("Select Query : {}", select_query);
+    println!("Select Values: {:?}", select_query.values);
+}
