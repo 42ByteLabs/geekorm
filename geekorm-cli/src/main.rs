56f0dfe4d58e1fda7de61571bf15c61e89cb3ac8
GeekMasher <geekmasher@gmail.com>
2024-03-28 12:54:35 +0000

    feat(cli): Move CLI

--- geekorm-cli/src/main.rs
+++ geekorm-cli/src/main.rs
@@ -1,20 +1,41 @@
 #![allow(unused_imports, dead_code, unused_variables)]
+extern crate geekorm;
+
 use anyhow::Result;
 // Imports
-use geekorm::prelude::*;
+use geekorm::Tables;
+use geekorm::{ForeignKey, GeekTable, PrimaryKey, QueryOrder, Table, TableBuilder};
 
 #[derive(Debug, Default, GeekTable)]
 pub struct User {
+    pub id: PrimaryKey,
     pub name: String,
     pub age: i32,
     pub postcode: Option<String>,
+    pub profile_id: ForeignKey<Profile>,
+}
+
+#[derive(Debug, Default, GeekTable)]
+pub struct Profile {
+    pub random_name: PrimaryKey,
+    pub bio: String,
 }
 
 fn main() -> Result<()> {
     println!("Starting...\n");
+    let tables = Tables::init(vec![User, Profile]);
 
-    let table = User::table();
-    println!("Table :: {:?}\n", table);
+    println!("Tables :: {}", tables.tables.len());
+    for table in tables.tables {
+        println!("Table :: {:#?}", table.get_primary_key());
+    }
+    println!("");
+
+    let table_user = User::table();
+    // println!("Table :: {:#?}\n", table_user);
+
+    let table_profile = Profile::table();
+    // println!("Table :: {:#?}\n", table_profile);
 
     let query = User::create().build()?;
     println!("Create :: `{}`\n", query);
