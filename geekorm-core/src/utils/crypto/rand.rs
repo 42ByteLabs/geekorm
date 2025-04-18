24ed253bd6ab6a86e87cd9cc5095975f6a545629
GeekMasher <geekmasher@gmail.com>
2025-04-18 14:51:30 +0100

    feat(core): Update to crypto v0.9

--- geekorm-core/src/utils/crypto/rand.rs
+++ geekorm-core/src/utils/crypto/rand.rs
@@ -7,11 +7,11 @@ pub const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
 
 /// Generate a random string of a given length
 pub fn generate_random_string(length: usize, prefix: impl Into<String>) -> String {
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
     let mut random_string = String::new();
     // Generate a random string of the given length using uppercase, lowercase and numbers
     for _ in 0..length {
-        let random_char = CHARSET[rng.gen_range(0..CHARSET.len())] as char;
+        let random_char = CHARSET[rng.random_range(0..CHARSET.len())] as char;
         random_string.push(random_char);
     }
     prefix.into() + &random_string
