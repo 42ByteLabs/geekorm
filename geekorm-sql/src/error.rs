bb46d8eba4bc2957c674cccba0745e5d807cbe4a
GeekMasher <geekmasher@gmail.com>
2025-08-02 21:31:06 +0100

    feat(sql): Update Values for SQL generation

--- geekorm-sql/src/error.rs
+++ geekorm-sql/src/error.rs
@@ -22,4 +22,20 @@ pub enum Error {
     /// IO error
     #[error("IO Error: {0}")]
     IoError(#[from] std::io::Error),
+
+    /// Version error
+    #[cfg(feature = "semver")]
+    #[error("Version Error: {0}")]
+    VersionError(#[from] semver::Error),
+
+    /// General error
+    #[error("Error: {0}")]
+    GeneralError(String),
+}
+
+impl Error {
+    /// Custom error constructor
+    pub(crate) fn custom(error: impl Into<String>) -> Self {
+        Error::GeneralError(error.into())
+    }
 }
